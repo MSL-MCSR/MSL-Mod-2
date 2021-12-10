47210207a24d086163653710ca143577fb085392
DuncanRL <59705125+DuncanRuns@users.noreply.github.com>
2021-12-10 04:14:57 -0400

    Remove 9000 casts from SeedCommandMixin

--- src/main/java/com/mcsrleague/mslmod/mixin/game/SeedCommandMixin.java
+++ src/main/java/com/mcsrleague/mslmod/mixin/game/SeedCommandMixin.java
@@ -2,7 +2,6 @@ package com.mcsrleague.mslmod.mixin.game;
 
 import com.mcsrleague.mslmod.session.SessionWorld;
 import com.mojang.brigadier.CommandDispatcher;
-import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.SeedCommand;
 import net.minecraft.server.command.ServerCommandSource;
@@ -19,18 +18,18 @@ public abstract class SeedCommandMixin {
      */
     @Overwrite
     public static void register(CommandDispatcher<ServerCommandSource> dispatcher, boolean dedicated) {
-        dispatcher.register((LiteralArgumentBuilder) ((LiteralArgumentBuilder) CommandManager.literal("seed").requires((serverCommandSource) -> {
+        dispatcher.register((CommandManager.literal("seed").requires((serverCommandSource) -> {
             return !dedicated || serverCommandSource.hasPermissionLevel(2);
         })).executes((commandContext) -> {
-            long l = ((ServerCommandSource) commandContext.getSource()).getWorld().getSeed();
+            long l = commandContext.getSource().getWorld().getSeed();
             Text text = Texts.bracketed((new LiteralText(String.valueOf(l))).styled((style) -> {
                 return style.withColor(Formatting.GREEN).withClickEvent(new ClickEvent(ClickEvent.Action.COPY_TO_CLIPBOARD, String.valueOf(l))).setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new TranslatableText("chat.copy.click"))).withInsertion(String.valueOf(l));
             }));
             if (SessionWorld.isSessionWorld()) {
-                ((ServerCommandSource) commandContext.getSource()).sendFeedback(new TranslatableText("mcsrleague.game.noseed").formatted(Formatting.RED), false);
+                commandContext.getSource().sendFeedback(new TranslatableText("mcsrleague.game.noseed").formatted(Formatting.RED), false);
                 return 0;
             } else {
-                ((ServerCommandSource) commandContext.getSource()).sendFeedback(new TranslatableText("commands.seed.success", new Object[]{text}), false);
+                commandContext.getSource().sendFeedback(new TranslatableText("commands.seed.success", new Object[]{text}), false);
                 return (int) l;
             }
         }));
